2b3e282ff6b1c29dd4f25a0c3499fb1118f6da4e
Patrick Emery <patrick.emery@outlook.com>
2021-06-30 21:13:01 -0400

    Add documentation
    Signed-off-by: Patrick Emery <patrick.emery@outlook.com>

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,20 +13,25 @@ namespace WebRecipes.Controllers
         private readonly WebRecipesContext _context;
         private readonly ILogger<HomeController> _logger;
 
+        /// <summary>
+        /// The Index page controller for this app.
+        /// </summary>
+        /// <param name="logger">A basic logger for this application</param>
+        /// <param name="context">A DB context used to load recipes for the front page.</param>
         public HomeController(ILogger<HomeController> logger, WebRecipesContext context)
         {
             _logger = logger;
             _context = context;
         }
 
+        /// <summary>
+        /// The Index page for this app.
+        /// Accumulates recipes on the front page, with preview images.
+        /// </summary>
+        /// <returns>A view containing a front page with a few recipes.</returns>
         public async Task<IActionResult> Index()
         {
             return View(await _context.Recipe.ToListAsync());
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- wwwroot/js/quillCompat.js
+++ wwwroot/js/quillCompat.js
@@ -1,8 +1,10 @@
-﻿var quill;
+﻿// On page load, set up the Quiil editor.
+var quill;
 $(() => quill = new Quill('#quill', {
     theme: 'snow'
 }));
 
+// Pull out the first image for a preview, then submit the entire field as content.
 $("#form").on("submit", () => {
     $('#quillImage').val($("#quill > .ql-editor > * > img, image").first().attr('src'));
     $("#quillInput").val($("#quill > .ql-editor").html());
